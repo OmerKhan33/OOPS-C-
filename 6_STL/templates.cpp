6a71fa454e94e60826648df58ac3728dc12fc192
OmerKhan360 <omer.khan2384@gmail.com>
2024-07-21 23:33:45 +0500

    successfully

---
+++ 6_STL/templates.cpp
@@ -0,0 +1,4 @@
+// Templates are a feature of the C++ programming language that allows functions and classes to 
+// operate with generic types. This allows a function or class to work on many different data types 
+// without being rewritten for each one.
+
