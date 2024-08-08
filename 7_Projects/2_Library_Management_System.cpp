739303a7f0b943aaf21bda3ddfea804ac93c8dca
OmerKhan360 <omer.khan2384@gmail.com>
2024-08-08 23:39:27 +0500

    successfully

--- 7_Projects/2_Library_Management_System.cpp
+++ 7_Projects/2_Library_Management_System.cpp
@@ -52,3 +52,56 @@ public:
         cout << "Role: Librarian" << endl;
     }
 };
+
+class Book {
+private:
+    string title;
+    string author;
+    bool isAvailable;
+public:
+    Book(string t, string a, bool avail = true) : title(t), author(a), isAvailable(avail) {}
+
+    void setAvailability(bool avail) {
+        isAvailable = avail;
+    }
+    bool getAvailability() const {
+        return isAvailable;
+    }
+    string getTitle() const {
+        return title;
+    }
+    void display() const {
+        cout << "Title: " << title << ", Author: " << author << ", Available: " << (isAvailable ? "Yes" : "No") << endl;
+    }
+};
+
+class Library {
+private:
+    vector<Book> books;
+public:
+    void addBook(const Book& book) {
+        books.push_back(book);
+    }
+    void displayBooks() const {
+        for (const auto& book : books) {
+            book.display();
+        }
+    }
+    bool borrowBook(const string& title) {
+        for (auto& book : books) {
+            if (book.getTitle() == title && book.getAvailability()) {
+                book.setAvailability(false);
+                return true;
+            }
+        }
+        return false;
+    }
+    void returnBook(const string& title) {
+        for (auto& book : books) {
+            if (book.getTitle() == title) {
+                book.setAvailability(true);
+                break;
+            }
+        }
+    }
+};
