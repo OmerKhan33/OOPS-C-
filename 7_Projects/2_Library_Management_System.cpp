67569b852b15f4ade076f28f6d34cf60a21947b2
OmerKhan360 <omer.khan2384@gmail.com>
2024-08-08 23:38:49 +0500

    successfully

--- 7_Projects/2_Library_Management_System.cpp
+++ 7_Projects/2_Library_Management_System.cpp
@@ -1,7 +1,54 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
 using namespace std;
 
-int main()
-{
-    //code will be uploaded soon. Stay tuned.
-}
+// Base class for Users
+class User {
+protected:
+    string name;
+    string id;
+public:
+    User(string n, string i) : name(n), id(i) {}
+    virtual void display() const {
+        cout << "Name: " << name << ", ID: " << id << endl;
+    }
+    virtual ~User() {}
+};
+
+// Derived class for Members
+class Member : public User {
+private:
+    vector<string> borrowedBooks;
+public:
+    Member(string n, string i) : User(n, i) {}
+    void borrowBook(const string& book) {
+        borrowedBooks.push_back(book);
+    }
+    void returnBook(const string& book) {
+        auto it = find(borrowedBooks.begin(), borrowedBooks.end(), book);
+        if (it != borrowedBooks.end()) {
+            borrowedBooks.erase(it);
+        }
+    }
+    void display() const override {
+        User::display();
+        cout << "Borrowed Books: ";
+        for (const auto& book : borrowedBooks) {
+            cout << book << " ";
+        }
+        cout << endl;
+    }
+};
+
+// Derived class for Librarians
+class Librarian : public User {
+public:
+    Librarian(string n, string i) : User(n, i) {}
+    void display() const override {
+        User::display();
+        cout << "Role: Librarian" << endl;
+    }
+};
