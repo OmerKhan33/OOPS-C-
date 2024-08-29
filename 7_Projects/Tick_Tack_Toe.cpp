8960dda9aebf5e04c63d66a022de7ff8edf004b5
Omer Khan <168018694+OmerKhan33@users.noreply.github.com>
2024-08-29 20:39:35 +0500

    small changes made

--- 7_Projects/Tick_Tack_Toe.cpp
+++ 7_Projects/Tick_Tack_Toe.cpp
@@ -85,8 +85,10 @@ public:
     void makeMove(char board[3][3]) override 
     {
         // Simple AI: place in the first available spot
-        for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
+        for (int i = 0; i < 3; ++i) 
+        {
+            for (int j = 0; j < 3; ++j) 
+            {
                 if (board[i][j] == ' ') {
                     board[i][j] = symbol;
                     return;
